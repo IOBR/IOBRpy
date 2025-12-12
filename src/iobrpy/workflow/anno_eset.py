f9e50285697cbfcbe8150d6f7dbb2416b1036a84
Haonan Huang <158280554+hhn0527@users.noreply.github.com>
2025-12-12 07:14:20 -0800

    Align anno_eset mean mode tie-breaking with R

--- src/iobrpy/workflow/anno_eset.py
+++ src/iobrpy/workflow/anno_eset.py
@@ -198,7 +198,13 @@ def anno_eset(eset_df: pd.DataFrame,
 
     # Merge annotation (probe_id becomes a column)
     eset_reset = eset_filtered.reset_index().rename(columns={eset_filtered.index.name or 'index': 'probe_id'})
-    merged = pd.merge(annotation_filtered, eset_reset, on="probe_id", how="inner")
+    merged = pd.merge(
+        annotation_filtered,
+        eset_reset,
+        on="probe_id",
+        how="inner",
+        sort=True  # mimic R merge(sort = TRUE): deterministic order for tie-breaking
+    )
     # drop probe_id column (we use symbol as index)
     merged.drop(columns=["probe_id"], inplace=True)
 
@@ -219,7 +225,7 @@ def anno_eset(eset_df: pd.DataFrame,
             merged['_score'] = merged[data_cols].mean(axis=1, skipna=True)
 
         # keep highest scoring row per symbol
-        merged.sort_values('_score', ascending=False, inplace=True)
+        merged.sort_values('_score', ascending=False, inplace=True, kind='mergesort')
         merged.drop(columns=['_score'], inplace=True)
         merged.drop_duplicates(subset=['symbol'], keep='first', inplace=True)
 
